7ff29c0fb44ea93830c86689cccdf9836bba80c2
JCX-15 <103619987+JCX-15@users.noreply.github.com>
2023-04-02 17:28:09 -0600

    Delete view-clientes directory

--- view-clientes/js/controlador.js
+++
@@ -1,9 +0,0 @@
-
-function registrarUsuario() {
-    var nombre = document.getElementById('nombreRegis').value;
-    var numeroTel = document.getElementById('numeroRegis').value;
-    var email = document.getElementById('emailRegis').value;
-    var contrasena = document.getElementById('contrasenaRegis').value;
-   
-    console.log(nombre, numeroTel, email, contrasena,);
-}
