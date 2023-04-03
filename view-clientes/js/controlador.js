29b9f9854023551de17dfd363165824eea3f7a39
AJLS05 <33355533+AJLS05@users.noreply.github.com>
2023-04-02 19:59:42 -0600

    Actualización de view de clientes

---
+++ view-clientes/js/controlador.js
@@ -0,0 +1,9 @@
+
+function registrarUsuario() {
+    var nombre = document.getElementById('nombreRegis').value;
+    var numeroTel = document.getElementById('numeroRegis').value;
+    var email = document.getElementById('emailRegis').value;
+    var contrasena = document.getElementById('contrasenaRegis').value;
+   
+    console.log(nombre, numeroTel, email, contrasena,);
+}
